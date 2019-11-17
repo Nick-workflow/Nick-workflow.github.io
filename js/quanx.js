ebb22d01190f9f87b24ffca6b9e76fd78e4f627e
Nick <yangtaoeng@163.com>
2019-11-17 22:21:14 +0800

    Update quanx.js

--- js/quanx.js
+++ js/quanx.js
@@ -1,6 +1,6 @@
 /**
- * @supported A1F401EF8F95
+ * @supported A1F401EF8F95 E8F7033D43B6
  */
 var body = $response.body;
-body = '\/**\n * @supported A1F401EF\n*\/\n' + body;
+body = '\/**\n * @supported A1F401EF8F95 E8F7033D43B6\n*\/\n' + body;
 $done(body);
